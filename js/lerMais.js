3f40fe4e5318d36c5f795daad1f8be20866f1c9e
HugoSalesSilva <93701716+HugoSalesSilva@users.noreply.github.com>
2023-05-29 03:20:04 -0300

    Add files via upload

---
+++ js/lerMais.js
@@ -0,0 +1,15 @@
+function vermais() {
+    let pontos = document.getElementById("pontos");
+    let maisTexto = document.getElementById("mais");
+    let btnVermais = document.getElementById("btnVerMais");
+
+    if(pontos.style.display === "none"){
+        pontos.style.display="inline";
+        maisTexto.style.display="none";
+        btnVermais.innerHTML="Ver Mais";
+    }else{
+        pontos.style.display="none";
+        maisTexto.style.display="inline";
+        btnVermais.innerHTML="Ler menos";
+    }
+}
